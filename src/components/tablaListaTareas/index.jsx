fafb80e5e54bc8fb226e36daf269c800f0c1ceb7
CristianDelgado31 <cristian-31-10@hotmail.com>
2023-11-27 16:06:07 -0300

    cambios en checkbox

--- src/components/tablaListaTareas/index.jsx
+++ src/components/tablaListaTareas/index.jsx
@@ -10,113 +10,73 @@ import TablePagination from '@mui/material/TablePagination';
 import TableRow from '@mui/material/TableRow';
 import Checkbox from '@mui/material/Checkbox';
 import DeleteIcon from '@mui/icons-material/Delete';
-import EditIcon from '@mui/icons-material/Edit';
 
-// Define el componente funcional TaskList
 function TaskList({ taskItems, onDeleteClick }) {
-  // Define una clave para el almacenamiento local
   const SELECTED_ITEMS = 'selectedItems';
 
-  // Estado para almacenar los elementos seleccionados
-  const [selected, setSelected] = React.useState([]);
-
-  // Estado para manejar la paginación
+  const [completedTasks, setCompletedTasks] = React.useState([]);
   const [page, setPage] = React.useState(0);
-
-  // Estado para manejar la cantidad de filas por página
   const [rowsPerPage, setRowsPerPage] = React.useState(5);
 
-  // Cargar elementos seleccionados del almacenamiento local al montar el componente
   useEffect(() => {
-    // Obtiene los elementos seleccionados del almacenamiento local o un array vacío si no hay ninguno
     const storedSelected = JSON.parse(localStorage.getItem(SELECTED_ITEMS)) || [];
-    // Establece los elementos seleccionados en el estado
-    setSelected(storedSelected);
+    setCompletedTasks(storedSelected);
   }, []);
 
-  // Función para cambiar la página actual cuando se cambia de página
   const handleChangePage = (event, newPage) => {
     setPage(newPage);
   };
 
-  // Función para cambiar la cantidad de filas por página cuando se selecciona una nueva cantidad
   const handleChangeRowsPerPage = (event) => {
     setRowsPerPage(parseInt(event.target.value, 10));
     setPage(0);
   };
 
-  // Función para manejar el cambio del checkbox en una fila
   const handleCheckboxChange = (id) => {
-    // Crea una copia del array de elementos seleccionados
-    const updatedSelected = [...selected];
-    // Obtiene el índice del elemento en el array
-    const index = updatedSelected.indexOf(id);
+    const updatedCompletedTasks = [...completedTasks];
+    const index = updatedCompletedTasks.indexOf(id);
 
-    // Si el elemento ya está seleccionado, lo deselecciona
     if (index !== -1) {
-      updatedSelected.splice(index, 1);
+      updatedCompletedTasks.splice(index, 1);
     } else {
-      // Si el elemento no está seleccionado, lo selecciona
-      updatedSelected.push(id);
+      updatedCompletedTasks.push(id);
     }
 
-    // Actualiza el estado con los elementos seleccionados
-    setSelected(updatedSelected);
-    // Almacena los elementos seleccionados en el almacenamiento local
-    localStorage.setItem(SELECTED_ITEMS, JSON.stringify(updatedSelected));
+    setCompletedTasks(updatedCompletedTasks);
+    localStorage.setItem(SELECTED_ITEMS, JSON.stringify(updatedCompletedTasks));
   };
 
-  // Función para manejar el clic en el ícono de eliminación
   const handleDeleteClick = () => {
-    onDeleteClick(selected);
-    setSelected([]);
+    onDeleteClick(completedTasks);
+    setCompletedTasks([]);
     localStorage.removeItem(SELECTED_ITEMS);
   };
 
-  // Función para verificar si un elemento está seleccionado
-  const isSelected = (id) => selected.indexOf(id) !== -1;
+  const isTaskCompleted = (id) => completedTasks.indexOf(id) !== -1;
 
-  // Renderiza el componente de la TaskList
   return (
     <Box sx={{ width: '100%' }}>
       <TableContainer>
         <Table aria-labelledby="tableTitle" size="medium">
-          {/* Cabecera de la TaskList */}
           <TableHead>
             <TableRow>
-              <TableCell>
-                {/* Checkbox para seleccionar/deseleccionar todos */}
-                <Checkbox
-                  indeterminate={selected.length > 0 && selected.length < taskItems.length}
-                  checked={selected.length === taskItems.length}
-                  onChange={(event) => {
-                    if (event.target.checked) {
-                      setSelected(taskItems.map((row) => row.id));
-                    } else {
-                      setSelected([]);
-                    }
-                  }}
-                />
-              </TableCell>
+              <TableCell />
               <TableCell>Tarea</TableCell>
               <TableCell>Comentario</TableCell>
             </TableRow>
           </TableHead>
-          {/* Cuerpo de la TaskList */}
           <TableBody>
             {taskItems
               .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
               .map((row) => (
                 <TableRow
                   key={row.id}
-                  selected={isSelected(row.id)}
                   onClick={() => handleCheckboxChange(row.id)}
                   style={{ cursor: 'pointer' }}
                 >
-                  {/* Checkbox en cada fila para seleccionar/deseleccionar */}
                   <TableCell padding="checkbox">
                     <Checkbox
-                      checked={isSelected(row.id)}
+                      checked={isTaskCompleted(row.id)}
                       onChange={() => handleCheckboxChange(row.id)}
                     />
                   </TableCell>
@@ -127,23 +87,11 @@ function TaskList({ taskItems, onDeleteClick }) {
           </TableBody>
         </Table>
       </TableContainer>
-      {/* Ícono de eliminación */}
-      {selected.length > 0 && (
-        <DeleteIcon
-          color="error"
-          style={{ cursor: 'pointer', marginTop: '10px' }}
-          onClick={handleDeleteClick}
-        />
-      )}
-      {/* Ícono para editar, nose si usarlo */}
-      {/*selected.length > 0 && (
-        <EditIcon
-          color="primary"
-          style={{ cursor: 'pointer', marginTop: '10px' }}
-        />
-      )*/}
-
-      {/* Paginación de la TaskList */}
+      <DeleteIcon
+        color="error"
+        style={{ cursor: 'pointer', marginTop: '10px' }}
+        onClick={handleDeleteClick}
+      />
       <TablePagination
         rowsPerPageOptions={[5, 10, 25]}
         component="div"
@@ -157,11 +105,9 @@ function TaskList({ taskItems, onDeleteClick }) {
   );
 }
 
-// Propiedades del componente
 TaskList.propTypes = {
   taskItems: PropTypes.array.isRequired,
   onDeleteClick: PropTypes.func.isRequired,
 };
 
-// Exporta el componente
 export default TaskList;
